87d53e2e14711a6c80dc9b191eda28679faa9217
Cheeseorim <121790715+Cheeseorim@users.noreply.github.com>
2023-05-14 00:23:38 +0900

    realfinal

--- result.php
+++ result.php
@@ -9,28 +9,12 @@
       <h1>마법의 소라고둥</h1>
     </header>
     <main>
-        <?php
-            $answers = [
-                '언젠가는',
-                '가만히 있어라',
-                '안 돼',
-                '그래',
-                '좋아',
-                '다시 한 번 물어봐',
-                '조심해',
-                '아니',
-                '응',
-                '물론',
-                '절대 아니야'
-            ];
-
-            $question = $_POST['question'];
-            $answer = $answers[array_rand($answers)];
-
-            echo "<p class='answer'>$answer</p>";
-            echo "<img src='magicsora2.jfif' alt='마법의 소라고둥 이미지'>";
-            echo "<p class='question'>$question</p>";
-        ?>
+        <p class="answer" id="answer"></p>
+        <img src="magicsora2.jfif" alt="마법의 소라고둥 이미지">
+        <p class="question"><?php echo $_POST["question"]; ?></p>
+        <div class="textbox">
+            <p><?php echo $answer; ?></p>
+          </div>
     </main>
     <footer>
       <p>문의 및 아이디어 : <a href="mailto:cheeseorim@gmail.com">cheeseorim@gmail.com</a></p>
--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-function getRandomAnswer() {
+function getAnswer() {
     const answers = [
-      '언젠가는',
-      '가만히 있어라',
-      '안 돼',
-      '그래',
-      '좋아',
-      '다시 한 번 물어봐',
-      '조심해',
-      '아니',
-      '응',
-      '물론',
-      '절대 아니야'
+        '언젠가는',
+        '가만히 있어라',
+        '안 돼',
+        '그래',
+        '좋아',
+        '다시 한 번 물어봐',
+        '조심해',
+        '아니',
+        '응',
+        '물론',
+        '절대 아니야'
     ];
-  
     const randomIndex = Math.floor(Math.random() * answers.length);
     return answers[randomIndex];
-  }
-  
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const question = urlParams.get('question');
-  const answer = getRandomAnswer();
-  
-  document.getElementById('question').textContent = question;
-  document.getElementById('answer').textContent = answer;
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const question = urlParams.get('question');
+const answer = getAnswer();
+
+document.getElementById('question').textContent = question;
+document.getElementById('answer').textContent = answer;
