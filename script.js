f5502c2fe3bff1659ce98d0f5be25e440d4460b4
Cheeseorim <121790715+Cheeseorim@users.noreply.github.com>
2023-05-14 00:32:29 +0900

    newver

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-function getAnswer() {
-    const answers = [
-        '언젠가는',
-        '가만히 있어라',
-        '안 돼',
-        '그래',
-        '좋아',
-        '다시 한 번 물어봐',
-        '조심해',
-        '아니',
-        '응',
-        '물론',
-        '절대 아니야'
-    ];
+const answers = [
+    "언젠가는",
+    "가만히 있어라",
+    "안 돼",
+    "그래",
+    "좋아",
+    "다시 한 번 물어봐",
+    "조심해",
+    "아니",
+    "응",
+    "물론",
+    "절대 아니야"
+  ];
+  
+  function getRandomAnswer() {
     const randomIndex = Math.floor(Math.random() * answers.length);
     return answers[randomIndex];
-}
-
-const urlParams = new URLSearchParams(window.location.search);
-const question = urlParams.get('question');
-const answer = getAnswer();
-
-document.getElementById('question').textContent = question;
-document.getElementById('answer').textContent = answer;
+  }
+  
+  const question = new URLSearchParams(window.location.search).get("question");
+  document.getElementById("question").innerText = question;
+  document.getElementById("answer-box").innerText = getRandomAnswer();
+  
