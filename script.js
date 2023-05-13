c06e072cc7ed5f31e00e5d8e358ad82a95800826
Cheeseorim <121790715+Cheeseorim@users.noreply.github.com>
2023-05-14 01:02:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,23 +13,23 @@ const answers = [
 ];
 
 function getRandomAnswer() {
-  const randomIndex = Math.floor(Math.random() * answers.length);
-  return answers[randomIndex];
-}
-
-// URL에서 질문 가져오기
-const urlParams = new URLSearchParams(window.location.search);
-const question = urlParams.get('question');
-document.getElementById('question').innerHTML = question;
-
-// 가져온 질문 보여주기
-const questionElement = document.createElement('p');
-questionElement.innerHTML = question;
-questionElement.classList.add('question');
-document.querySelector('main').insertBefore(questionElement, document.querySelector('img'));
-
-// 랜덤 답변 보여주기
-const answerBox = document.getElementById('answer-box');
-const answerElement = document.createElement('p');
-answerElement.innerHTML = getRandomAnswer();
-answerBox.appendChild(answerElement);
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    return answers[randomIndex];
+  }
+  
+  // URL에서 질문 가져오기
+  const urlParams = new URLSearchParams(window.location.search);
+  const question = urlParams.get('question');
+  
+  // 가져온 질문 보여주기
+  const questionElement = document.createElement('p');
+  questionElement.innerHTML = question;
+  questionElement.classList.add('question');
+  document.querySelector('main').insertBefore(questionElement, document.querySelector('img'));
+  
+  // 랜덤 답변 보여주기
+  const answerBox = document.getElementById('answer-box');
+  const answerElement = document.createElement('p');
+  answerElement.innerHTML = getRandomAnswer();
+  answerBox.appendChild(answerElement);
+  
